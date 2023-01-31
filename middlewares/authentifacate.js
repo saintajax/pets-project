55db22dc2de9a5ccf50307b866c251e0933c2b1d
OlegKushnir <kushnir.oleg87@gmail.com>
2023-01-31 14:24:52 +0200

    fix

--- middlewares/authentifacate.js
+++ middlewares/authentifacate.js
@@ -1,29 +1,29 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../service/schemas/user");
-const {
-  NotAutorizedError
-} = require("../helpers/authErrors");
+const { NotAutorizedError } = require("../helpers/authErrors");
 
 const authMiddleware = async (req, res, next) => {
   try {
-
-  const {authorization} = req.headers;
-  if (!authorization) {
-    next(new NotAutorizedError("Not authorized"));
-  }
-  const [, token] = authorization.split(' ');
-  if (!token) {
-    next(new NotAutorizedError("Not authorized"));
-  }
+    const { authorization } = req.headers;
+    if (!authorization) {
+      next(new NotAutorizedError("Not authorized"));
+    }
+    const [, token] = authorization.split(" ");
+    if (!token) {
+      next(new NotAutorizedError("Not authorized"));
+    }
     const tokenUser = jwt.decode(token, process.env.SECRET_KEY);
+    if (!tokenUser._id) {
+      next(new NotAutorizedError("Not authorized"));
+    }
     const dbUser = await User.findById(tokenUser._id);
-  
+
     if (!dbUser) next(new NotAutorizedError("Not authorized"));
     if (dbUser.token !== token) next(new NotAutorizedError("Not authorized"));
 
     req.token = token;
     req.user = dbUser;
-    
+
     next();
   } catch (err) {
     console.log(err);
@@ -32,5 +32,5 @@ const authMiddleware = async (req, res, next) => {
 };
 
 module.exports = {
-  authMiddleware
+  authMiddleware,
 };
