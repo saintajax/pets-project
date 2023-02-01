17ab9394e166fce1e15307deac6a0a4ea4d1f7e3
saintajax <saintajax@gmail.com>
2023-02-01 13:32:29 +0200

    base swagger docs and user routs

---
+++ api/auth.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { authMiddleware } = require("../middlewares/authentifacate");
+const { validateBody } = require("../middlewares/validateBody");
+const { catchWrapper } = require("../helpers/errorCatcher");
+const { registerSchema, loginSchema } = require("../service/schemas/user");
+const {
+  register,
+  login,
+  logout,
+  verifyEmailController,
+  repeatEmailController,
+} = require("../controller/authController");
+
+const router = express.Router();
+
+router.post("/register", validateBody(registerSchema), catchWrapper(register));
+router.post("/login", validateBody(loginSchema), catchWrapper(login));
+
+router.post("/logout", authMiddleware, catchWrapper(logout));
+router.post("/verify", catchWrapper(repeatEmailController));
+router.get("/verify/:verificationToken", catchWrapper(verifyEmailController));
+
+module.exports = router;
--- api/user.js
+++ api/user.js
@@ -3,37 +3,22 @@ const { authMiddleware } = require("../middlewares/authentifacate");
 const { validateBody } = require("../middlewares/validateBody");
 const upload = require("../middlewares/upload");
 const { catchWrapper } = require("../helpers/errorCatcher");
+const { updateSchema } = require("../service/schemas/user");
 const {
-  registerSchema,
-  loginSchema,
-  updateSchema,
-} = require("../service/schemas/user");
-const {
-  register,
-  login,
-  logout,
   update,
-  verifyEmailController,
-  repeatEmailController,
+
   getCurrent,
 } = require("../controller/authController");
 
 const router = express.Router();
 
-router.post("/register", validateBody(registerSchema), catchWrapper(register));
-router.post("/login", validateBody(loginSchema), catchWrapper(login));
-
-router.post(
+router.patch(
   "/update",
   authMiddleware,
   upload.single("avatar"),
   validateBody(updateSchema),
   catchWrapper(update)
 );
-
-router.post("/logout", authMiddleware, catchWrapper(logout));
-router.post("/verify", catchWrapper(repeatEmailController));
-router.get("/verify/:verificationToken", catchWrapper(verifyEmailController));
 router.get("/current", authMiddleware, catchWrapper(getCurrent));
 
 module.exports = router;
