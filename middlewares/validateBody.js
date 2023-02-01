66d713205f5581a06d7e75d1bc22e626a55e7240
OlegKushnir <kushnir.oleg87@gmail.com>
2023-02-01 18:06:01 +0200

    v

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -12,3 +12,19 @@
 //   return fn;
 // };
 // module.exports = { validateBody };
+const Joi = require("joi");
+const { ValidationError } = require("../helpers/authErrors");
+
+
+const validateBody = (schema) => {
+  return async (req, res, next) => {
+    try {
+      const validated = await schema.validateAsync(req.body);
+      req.body = validated;
+      next();
+    } catch (err) {
+      next(new ValidationError(err));
+    }
+  };
+};
+module.exports = { validateBody };
